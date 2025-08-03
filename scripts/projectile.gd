173f15763d856bf323e4502ace49c84d0a610a75
Theo Ratkin <theo@theoratkin.com>
2025-08-03 12:41:52 -0400

    fix(projectile): fly past the target and add destroy timer

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -3,8 +3,14 @@ extends Area2D
 @export var speed = 100
 @export var target = Vector2()
 
+var target_direction = Vector2()
+
+func _ready():
+	target_direction = global_position.direction_to(target) * 500
+
+
 func _process(delta: float):
-	global_position = global_position.move_toward(target, delta * speed)
+	global_position = global_position.move_toward(target_direction, delta * speed)
 
 
 func destroy():
@@ -15,3 +21,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if "on_hit" in body:
 		body.on_hit(global_position)
 		call_deferred("destroy")
+
+
+func _on_destroy_timer_timeout() -> void:
+	destroy()
