6ed4555a40c832ad9cbcce3b06d4516aba7585a7
Theo Ratkin <theo@theoratkin.com>
2025-08-03 12:45:57 -0400

    fix: spawn enemies inside a single container

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var enemies_container = get_node("/root/main/enemies container")
+
 var enemy_pool: Array[Resource] = [
 	preload("res://reusable_scenes/evil_green_thing.tscn"),
 	preload("res://reusable_scenes/evil_red_thing.tscn")
@@ -8,4 +10,5 @@ var enemy_pool: Array[Resource] = [
 
 func spawn_random_enemy():
 	var instance = enemy_pool.pick_random().instantiate()
-	add_child(instance)
+	instance.global_position = global_position
+	enemies_container.add_child(instance)
