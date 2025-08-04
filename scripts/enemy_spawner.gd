134c84ed1983152bb30069ef6dc2c29496c8b41a
Theo Ratkin <theo@theoratkin.com>
2025-08-04 13:41:38 -0400

    feat: add fire enemy

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -4,7 +4,8 @@ extends Node2D
 
 var enemy_pool: Array[Resource] = [
 	preload("res://reusable_scenes/evil_green_thing.tscn"),
-	preload("res://reusable_scenes/evil_red_thing.tscn")
+	preload("res://reusable_scenes/evil_red_thing.tscn"),
+	preload("res://reusable_scenes/evil_fire_thing.tscn")
 ]
 
 
